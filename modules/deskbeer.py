60ec8e2c0e1f95be4706313941bd73746b7c3ee3
Kevin Morgan <kevin.morgan@hogarthww.com>
2011-08-26 16:42:55 +0100

    Changing deskbeer.

--- modules/deskbeer.py
+++ modules/deskbeer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
-def deskbeer(phenny, stuff):
-	phenny.say('Soon....')
+def deskbeer(phenny, word):
+    if word == 'when':
+        phenny.say('Soon...')
+    elif word == 'who':
+        phenny.say("Not sure yet. Be nice to me and then we'll see")
+    else:
+        phenny.say('Pffft......')
 deskbeer.commands = ['deskbeer']
 deskbeer.example = '.deskbeer'
 
