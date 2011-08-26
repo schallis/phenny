2fb2c4ed49bcb35e5ad45868d62d273edad9e269
Kevin Morgan <kevin.morgan@hogarthww.com>
2011-08-26 16:19:08 +0100

    Adding deskbeer command.

---
+++ modules/deskbeer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+def deskbeer(phenny):
+	phenny.say('Soon....')
+deskbeer.commands = ['deskbeer']
+deskbeer.example = '.deskbeer'
+
+if __name__ == '__main__':
+	print __doc__.strip()
