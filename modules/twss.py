b58cf7d13049e69e36157f4a1392f496d3b73cae
Steve Challis <steven.challis@hogarthww.com>
2012-09-21 14:17:45 +0100

    Adding twss plugin

---
+++ modules/twss.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+def twss(phenny, input):
+    phenny.say("THAT'S WHAT SHE SAID!")
+twss.rule = r'.*(big|small|long|hard|soft|mouth|face|good|fast|slow|in there|on there|in that|on that|wet|dry|on the|in the|suck|blow|jaw|all in|fit that|fit it|hurts|hot|huge|balls|stuck)'
+twss.priority = 'low'
+
